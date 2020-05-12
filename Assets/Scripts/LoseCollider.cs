e1815322939f3070a17d66f313aa19115bccbdc7
David Andrus <davidandrus.com@gmail.com>
2020-05-11 23:42:08 -0700

    Getting paddle moving and clamped to edges

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -8,6 +8,6 @@ public class LoseCollider : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        SceneManager.LoadScene("Game Over", LoadSceneMode.Single);
+      SceneManager.LoadScene("Game Over", LoadSceneMode.Single);
     }
 }
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float clampOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseToUnitPos = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        float clampedPos = Mathf.Clamp(mouseToUnitPos, clampOffset, screenWidthInUnits - clampOffset);
+        Vector2 paddlePos = new Vector2(clampedPos, this.transform.position.y);
+        this.transform.position = paddlePos;
+    }
+}
