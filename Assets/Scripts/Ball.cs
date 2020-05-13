bb428d8c53264bd5252288ac5daee0ca81c77f6d
David Andrus <davidandrus.com@gmail.com>
2020-05-13 07:08:47 -0700

    Add sound effect to ball and reduce friction

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,21 +2,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class Ball : MonoBehaviour
 {
     // config parameters
     [SerializeField] Paddle paddle1;
     [SerializeField] Vector2 startVelocity;
+    [SerializeField] AudioClip[] ballSounds;
 
     // state
     Vector2 paddleToBallVector;
     bool hasStarted = false;
-    
+
+    //cached component references
+    AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         paddleToBallVector = transform.position - paddle1.transform.position;
     }
 
@@ -45,5 +49,14 @@ public class Ball : MonoBehaviour
 
         // start ball on paddle
         transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasStarted)
+        {
+            AudioClip randomSound = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            audioSource.PlayOneShot(randomSound);
+        }
     }
 }
