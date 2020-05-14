7728bb377eca932fcd10378c3f45667bd780eda7
David Andrus <davidandrus.com@gmail.com>
2020-05-14 06:41:49 -0700

    Increment current score on when block breaks

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -8,14 +8,17 @@ public class Block : MonoBehaviour
     [SerializeField] AudioClip blockBreakSound;
     [SerializeField] Level level;
 
+
     private void Start()
     {
+       
         level = FindObjectOfType<Level>();
         level.CountBreakableBlocks();
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
+        FindObjectOfType<GameStatus>().AddToScore();
         Destroy(gameObject);
         AudioSource.PlayClipAtPoint(blockBreakSound, Camera.main.transform.position);
         level.BlockDestroyed();
