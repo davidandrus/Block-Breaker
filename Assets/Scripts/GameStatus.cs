dc5f6114e65a017001916707c4cf10522be8011c
David Andrus <davidandrus.com@gmail.com>
2020-05-14 06:53:10 -0700

    Adding score to screen and updating score

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameStatus : MonoBehaviour
 {
     // config params
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] Text scoreText;
 
     // state variables
     [SerializeField ] int currentScore = 0;
 
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
 
     // Update is called once per frame
     void Update()
@@ -21,5 +28,6 @@ public class GameStatus : MonoBehaviour
     public void AddToScore ()
     {
         currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
     }
 }
